ce637ecf867b4e6b17e6f69bdfdef7ade08a02c4
Naira Petrosyan <nairapetrosya@gmail.com>
2018-01-15 10:34:10 +0400

    fifth commit

--- index.js
+++ index.js
@@ -2,38 +2,28 @@ const moment = require('moment');
 const fs = require('fs');
 var endOfLine = require('os').EOL;
 const { Writable, Readable, Transform} = require('stream');
+moment.suppressDeprecationWarnings = true;
 
-const w =fs.createWriteStream('./dates.txt');
+/*const w =fs.createWriteStream('./dates.txt');
+setInterval(()=>{
+    w.write(moment().toString().concat(endOfLine), (err, b)=>{
+        if(err)
+            throw err;
+        console.log("success");
+    });
+}, 1000);*/
 
-// setInterval(()=>{
-//     w.write(moment().toString().concat(endOfLine), (err, b)=>{
-//         if(err)
-//             throw err;
-//         console.log("success");
-//     });
-// }, 1000);
 
-console.log(moment.duration(1000));
-
-// moment.duration(1000).(function() {
-//     w.write(moment().toString().concat(endOfLine), (err, b)=>{
-//         if(err)
-//             throw err;
-//         console.log("success");
-//         });
-//   });
 
 
 class MyReadable extends Readable {
     constructor(){
         super();
-
-
     }
-    _read(size){
-        
-        this.push((new Date()).toString());
-
+    _read(size){  
+      setTimeout(()=>{
+          this.push((new Date()).toString());
+        }, 1000);
     }
 }
 
@@ -42,25 +32,30 @@ class MyWritable extends Writable {
     constructor(filepath) {
       super();
       this.fd = fs.openSync(filepath, 'w+');
-      console.log(this.fd, 'aaaaaaaaaaaaaaaaaa');
     }
     _write(chunk, encoding, callback) {
-        //console.log(this.fd);
-        fs.write(this.fd, chunk, (err, fd)=>{if(err) throw err; console.log('success')});
+        fs.write(this.fd, chunk,  (err)=>{
+          if(err) 
+            throw err;
+          console.log('success')});
         callback();
     }
   }
+
+
 
   class MyTransform extends Transform {
     constructor() {
       super();
     }
     _transform(chunk, encoding, callback) {
+        this.push(moment(chunk.toString()).toString().concat(endOfLine));
+      callback();
     }
   }
 
   const myRStream = new MyReadable();
   const myWStream = new MyWritable('./dates.txt');
+  const myTStream = new MyTransform();
 
-  myRStream.pipe(myWStream);
-
+  myRStream.pipe(myTStream).pipe(myWStream);
